9cb54d5650bd4c9434bec351b439fa36201929a7
David Lorente <david.l.castilla@gmail.com>
2019-01-18 17:13:32 -0500

    fix axis ticks

--- src/barchart/components/YAxis.js
+++ src/barchart/components/YAxis.js
@@ -3,14 +3,16 @@ import Tick from './Tick';
 
 const YAxis = (props) => {
 	const {nTicks, range} = props
-	const tickVal = (range[1]-range[0])/nTicks
+	const tickVal = (range[1]-range[0])/(nTicks - 1)
+
 	return (
 		<g transform={`translate(${props.yPos},${props.xPos})`} fill="none" font-size="10" font-family="sans-serif" text-anchor="middle">
 			<path className="domain" stroke="currentColor" d={`M-0.5,0V${props.height}`}></path>
 			{[...Array(nTicks).keys()].map((i) => 
 				<Tick 
-					text={range[1] - (tickVal*i)}
-					y={props.height/(nTicks) * i}
+					text={parseInt(range[1]-(tickVal*i))}
+					y={props.height/(nTicks-1) * i}
+					axis="y"
 				/>
 			)}
 		</g>
